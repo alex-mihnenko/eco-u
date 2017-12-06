7ce04667d8a833091374a9d9d93a095e691b03b9
6key <voroshilov@gmail.com>
2017-12-06 08:24:02 +0300

    Должно убрать вывод предупреждений, если товар отключен

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -36,11 +36,10 @@ class ModelCatalogProduct extends Model {
                             }
                         }
                     }
-                
-                
-                $arCompositePrice = $this->config->get('config_composite_price');
-                $compPrice = false;
-                if($query->row['composite_price'] == 1) {
+               
+                if($query->num_rows && $query->row['composite_price'] == 1) {
+                    $arCompositePrice = $this->config->get('config_composite_price');
+                    $compPrice = false;
                     if($query->row['weight_class'] == 'кг' || $query->row['weight_class'] == 'л') {
                         $compPrice = $arCompositePrice;
                     } elseif($query->row['weight_class'] == 'г' || $query->row['weight_class'] == 'мл') {
