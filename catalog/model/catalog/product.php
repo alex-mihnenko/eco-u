a757c9d67ad0782c9e94c664343c1ce056172d97
6key <voroshilov@gmail.com>
2017-12-14 02:59:46 +0300

    Правильный вывод товаров для поиска

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -271,7 +271,9 @@ class ModelCatalogProduct extends Model {
             foreach($items as $item) {
                 if(!isset($result[$item['product_id']])) {
                     $arProduct = $this->getProduct($item['product_id']);
-                    if($arProduct['status'] != 1) continue;
+                    if(($arProduct['quantity'] <= 0 && $arProduct['stock_status_id'] == 5) || $arProduct['status'] != 1) {
+                        continue;
+                    }
                     $result[$item['product_id']] = $arProduct;
                     $result[$item['product_id']]['href'] = $this->url->link('product/product', '&product_id=' . $item['product_id']);
                 }
