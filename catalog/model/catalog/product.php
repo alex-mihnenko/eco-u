31cd57c0aa48c01c7206a1286c2eb0d61cc8dbcf
6key <voroshilov@gmail.com>
2018-01-04 16:04:07 +0300

    Правки по тз

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -357,7 +357,7 @@ class ModelCatalogProduct extends Model {
         public function getCatsortProducts($category_id, $not_include) {
             if(!empty($category_id)) {
                 $nInclude = implode(', ', $not_include);
-                $sql = "SELECT `product_id` FROM " . DB_PREFIX . "product_to_category pc WHERE `category_id` = ".(int)$category_id." AND `product_id` NOT IN (".$nInclude.") AND product_id = (SELECT product_id FROM ".DB_PREFIX."product pd WHERE pc.product_id = pd.product_id AND status != 0 AND (stock_status_id != 5 || quantity > 0)) LIMIT 0, 10000";
+                $sql = "SELECT `product_id`, (SELECT `name` FROM ".DB_PREFIX."product_description pn WHERE pn.product_id = pc.product_id) AS `name` FROM " . DB_PREFIX . "product_to_category pc WHERE `category_id` = ".(int)$category_id." AND `product_id` NOT IN (".$nInclude.") AND product_id = (SELECT product_id FROM ".DB_PREFIX."product pd WHERE pc.product_id = pd.product_id AND status != 0 AND (stock_status_id != 5 || quantity > 0)) ORDER BY pn.name LIMIT 0, 10000";
                 $query = $this->db->query($sql);
                 foreach($query->rows as $i => $row) {
                     $product = $this->getProduct($row['product_id']);
