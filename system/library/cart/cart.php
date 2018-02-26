0ee035ed5e773de84793a5460dde918ce6626a9c
Kostya <konstantin.ohrimenko@gmail.com>
2018-02-26 14:26:41 +0200

    Cart Bug Fix

--- system/library/cart/cart.php
+++ system/library/cart/cart.php
@@ -239,7 +239,7 @@ class Cart {
                                     $cPrice = $this->config->get('config_composite_price');
                                     $wVariants = explode(',', $product_query->row['weight_variants']);
                                     $wKey = $cart['weight_variant'];
-                                    if(isset($cPrice[$wVariants[$wKey]])) {
+                                    if(isset($wVariants[$wKey]) && isset($cPrice[$wVariants[$wKey]])) {
                                         $price = $price * $cPrice[$wVariants[$wKey]];
                                     }
                                 }
