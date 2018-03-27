84f21fe611805a2b4418339339c8f28dc03815dd
Kostya <konstantin.ohrimenko@gmail.com>
2018-03-27 17:29:35 +0300

    Fix SendSMS

--- catalog/controller/extension/payment/rbs.php
+++ catalog/controller/extension/payment/rbs.php
@@ -94,7 +94,7 @@ class ControllerExtensionPaymentRbs extends Controller {
                 
                 $this->load->model('sms/confirmation');
                 $message = str_replace('[REPLACE]', $order_number, $this->config->get('config_sms_order_new_text'));
-                $this->model_sms_confirmation->sendSMS($$order_info['telephone'], $message);
+                $this->model_sms_confirmation->sendSMS($order_info['telephone'], $message);
                 
 //                $this->response->redirect($this->url->link('checkout/success', '', true));
                 $this->session->data['success_order_id'] = $order_number;
