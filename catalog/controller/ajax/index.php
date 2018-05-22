185a13e19fca724b0f9df6b7bb6935b0416bc6f9
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-05-22 16:49:08 +0300

    Fix ajaxAddOrder

--- catalog/controller/ajax/index.php
+++ catalog/controller/ajax/index.php
@@ -192,12 +192,14 @@ class ControllerAjaxIndex extends Controller {
           $data['products'] = $this->cart->getProducts();
           $total = 0;
           foreach($data['products'] as $i => $product) {
+              $data['products'][$i]['quantity'] = $product['quantity']*$product['packaging'];
+
               if(empty($product['weight_variants'])) {
-                  $data['products'][$i]['amount'] = $product['quantity'];
+                  $data['products'][$i]['amount'] = round($product['quantity']*$product['packaging']);
                   $data['products'][$i]['variant'] = 1;
               } else {
                   $arWeightVariants = explode(',', $product['weight_variants']);
-                  $data['products'][$i]['amount'] = $product['quantity'];
+                  $data['products'][$i]['amount'] = round( ($product['quantity']*$product['packaging']) /$arWeightVariants[$product['weight_variant']]);
                   $data['products'][$i]['variant'] = $arWeightVariants[$product['weight_variant']];
               }
               $total += ($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')) * $product['quantity']);
