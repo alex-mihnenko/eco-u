271e1800bc0cd12b879d2d7ed56a42b95d74a52f
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-05-23 15:40:59 +0300

    Change weight_package saving

--- ms+crm/opencart_ms_ord.php
+++ ms+crm/opencart_ms_ord.php
@@ -156,7 +156,7 @@ while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$c
 
 				// Add package weight
 					if( $weight_package != '' ) {
-						var_dump($weight_package);
+						var_dump(htmlentities($weight_package));
 						echo "<br>";
 						
 						$wpArr = (array) json_decode($weight_package);
