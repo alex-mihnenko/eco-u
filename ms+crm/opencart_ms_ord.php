6df948c0228490f336ee608a721d26e0beb0da42
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-05-23 15:22:56 +0300

    #194 - Add package weight

--- ms+crm/opencart_ms_ord.php
+++ ms+crm/opencart_ms_ord.php
@@ -156,25 +156,22 @@ while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$c
 
 				// Add package weight
 					if( $weight_package != '' ) {
-						
-						$weight_package = (array)json_decode($weight_package);
+						$weight_package = json_decode($weight_package);
 						print_r($weight_package);
-						echo '<br>';
-						
-						$weight_package_index = strval($fasovka);
-						
-						echo $weight_package_index.' --- '.$weight_package["$weight_package_index"];
-						echo '<br>';
 
-						if( isset($weight_package["$weight_package_index"]) ) {
+						$weight_package_index = strval($fasovka);
+						echo $weight_package_index.' --- '.$weight_package[$weight_package_index];
+						echo "<br>";
+
+						if( isset($weight_package[$weight_package_index]) ) {
 							// g
 							if($weight_class_id==2) {
-								$weight_all=$weight_all + floatval($weight_package["$weight_package_index"]);
+								$weight_all=$weight_all + floatval($weight_package[$weight_package_index]);
 							}
 
 							// kg
 							if($weight_class_id==9) {
-								$weight_all=$weight_all + floatval($weight_package["$weight_package_index"])*1000;
+								$weight_all=$weight_all + floatval($weight_package[$weight_package_index])*1000;
 							}
 						}
 					}
