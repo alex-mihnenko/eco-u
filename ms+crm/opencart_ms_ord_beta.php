13a018934b6c6b5c393467658bf6ae296e66630f
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-05-23 12:57:55 +0300

    Server change ms+crm/opencart_ms_ord.php

--- ms+crm/opencart_ms_ord_beta.php
+++ ms+crm/opencart_ms_ord_beta.php
@@ -1,7 +1,7 @@
 <?php
 // Init
-	#error_reporting(E_ALL);
-	#ini_set('display_errors', '1');
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
 
 	include("opencart_inc.php");
 
@@ -21,10 +21,31 @@ $res_orders=mysql_query("
 
 $i=1;
 
-while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$comm,$total,$order_status_id,$date_added,$shipping_code,$shipping_postcode,$shipping_city,
+
+
+// Get free shipping
+	$freeShippingTotalValue = 1000000;
+
+	// Get free
+		if ( $qFreeTotal = mysql_query("SELECT * FROM `oc_setting` WHERE `code`='free';") ) $nFreeTotal = mysql_num_rows($qFreeTotal);
+		else $nFreeTotal = 0;
+
+
+		if( $nFreeTotal>0 ){
+			// ---
+				while ($freeTotalRow = mysql_fetch_assoc($qFreeTotal)) {
+					if( $freeTotalRow['key'] == 'free_total' ) { $freeShippingTotalValue = $freeTotalRow['value']; }
+				}
+			// ---
+		}
+	// ---
 // ---
 
+while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$comm,$total,$order_status_id,$date_added,$shipping_code,$shipping_postcode,$shipping_city,
 	$shipping_country,$shipping_address_1,$shipping_address_2,$delivery_time)=mysql_fetch_row($res_orders)){
+	
+// ---
+	$orderCreatedAt = $date_added;
 	
 	// Check email
 	if($email == 'empty@localhost' || $email=='') {
@@ -41,9 +62,11 @@ while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$c
 	if(!isset($roistat_id)) $roistat_id="";
 	
 	
-	$res=mysql_query("select value from oc_order_total where code='shipping' and order_id='$order_id'");
-	list($deliveryCost)=mysql_fetch_row($res);
-	if(!isset($deliveryCost)) $deliveryCost=0;
+	// Get delivery cost
+		$res=mysql_query("select value from oc_order_total where code='shipping' and order_id='$order_id'");
+		list($deliveryCost)=mysql_fetch_row($res);
+		if(!isset($deliveryCost)) $deliveryCost=0;
+	// ---
 
 	// Customer
 		$ms_last_tmp=DateTime::createFromFormat("Y-m-d H:i:s",$date_added);
@@ -97,17 +120,18 @@ while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$c
 	
 		while(list( $ms_pr_id,$msp_product_id,$opid,$quantity,$fasovka,$amount,$price)=mysql_fetch_row($resx)){
 
-			$resx2=mysql_query("select MSV.ms_id,MSV.product_option_value_id  from oc_order_option as OOO, ms_variants as MSV where MSV.product_option_value_id=OOO.product_option_value_id and OOO.order_id='$order_id' 
-				and OOO.order_product_id='$opid'");
+			$resx2=mysql_query("select MSV.ms_id,MSV.product_option_value_id  from oc_order_option as OOO, ms_variants as MSV where MSV.product_option_value_id=OOO.product_option_value_id and OOO.order_id='$order_id' and OOO.order_product_id='$opid'");
 			list( $ms_var_id, $msv_povid)=mysql_fetch_row($resx2);
+
 			/*Подсчитываем общий вес всех товаров*/
+
 			//1.Получем единицу измерения товара
 			$resx3=mysql_query("select weight_class_id,weight from oc_product  where  product_id='".(int)$msp_product_id."'");
 			list( $weight_class_id, $weight)=mysql_fetch_row($resx3);
-			//echo $quantity." ".$weight."<br>";
-			//2.Формируем вес
 			
+			//2.Формируем вес
 			//Если в МС не установлен вес для весовых товаров, то берём по умолчанию 1 кг
+
 			if($weight=="0.00000000" && $weight_class_id==9){
 				$weight="1";
 			}
@@ -116,8 +140,7 @@ while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$c
 			if($weight=="0.00000000"){
 				$weight_ignore=1;
 			}
-			else 
-			{
+			else {
 				
 				if($weight_class_id==8 || $weight_class_id==2 || $weight_class_id==1 || $weight_class_id==7)
 				{
@@ -213,38 +236,73 @@ while(list($payment_method,$customer_id,$order_id,$fname,$lname,$email,$phone,$c
 		$delivery_code = $tmp[0];
 		$order['shipmentStore']='eco-u';
 		$vr=$delivery_code;
-		if($delivery_code=="mkadout")
-		{
+
+		if($delivery_code=="mkadout") {
 			$vr="mkad";
 		}
-		if($delivery_code=="free")
-		{
+		if($delivery_code=="free") {
 			$vr="flat";
 		}
-		if($delivery_code=="flat")
-		{
+		if($delivery_code=="flat") {
 			$vr="flat-pay";
 		}
 
+
+		// Get delivery net cost
+			$deliveryNetCost = 0;
+			
+			// Get current
+				if ( $qShippingNetCost = mysql_query("SELECT * FROM `oc_setting` WHERE `code`='".$delivery_code."';") ) $nShippingNetCost = mysql_num_rows($qShippingNetCost);
+				else $nShippingNetCost = 0;
+
+
+				if( $nShippingNetCost>0 ){
+					// ---
+						$mainCost = 0;
+						$netCost = 0;
+
+						while ($shippingNetCostRow = mysql_fetch_assoc($qShippingNetCost)) {
+							
+							if( $shippingNetCostRow['key'] == $delivery_code.'_cost' ) { $mainCost = $shippingNetCostRow['value']; }
+							if( $shippingNetCostRow['key'] == $delivery_code.'_netcost' ) { $netCost = $shippingNetCostRow['value']; }
+						
+						}
+
+						if($delivery_code=="flat") {
+							$deliveryNetCost = $netCost;
+						}
+						else{
+							// ---
+								if( $total_new >= $freeShippingTotalValue ){
+									// Free shipping
+										$deliveryNetCost = $netCost + $deliveryCost;
+									// ---
+								}
+								else{
+									// Paided shipping
+										$deliveryNetCost = $netCost + ($deliveryCost - $mainCost);
+									// ---
+								}
+							// ---
+						}
+
+					// ---
+				}
+			// ---
+		// ---
+
+
         $order['delivery'] = array(
-            // 'code' => !empty($delivery_code) ? $delivery_code:0, //$settings['retailcrm_delivery'][$delivery_code] : '',
-             'code' => !empty($vr) ? $vr:0, //$settings['retailcrm_delivery'][$delivery_code] : '',
-            // 'service'=>array(
-            //'deliveryType' => !empty($vr) ? $vr:0, 
-			//),
-            	'cost' => (double)$deliveryCost,
-            	'address' => array(
-				//'index' => $shipping_postcode,
-				//'city' => $shipping_city,
-                'text' => $add_text
-			)
-		
-		);				
+			'code' => !empty($vr) ? $vr:0,
+			'cost' => (double)$deliveryCost,
+			'netCost' => (double)$deliveryNetCost,
+			'address' => array('text' => $add_text)
+		);
     // ---
 
     // Init
 		$link='https://eco-u.retailcrm.ru/api/v5/orders/create?apiKey='.$retail_key;
-		$order['createdAt']=$date_added;
+		$order['createdAt']=$orderCreatedAt;
 	   	$order['items']=$items_new;
 		$order['number']='IM'.$order_id;
 		if($weight_ignore==0) { $order['weight']=$weight_all; }
