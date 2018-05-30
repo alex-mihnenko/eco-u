022647fe770768e01cc2a17fd29fa401642e8073
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-05-30 15:34:20 +0300

    Fix links without descriptions

--- catalog/controller/extension/module/featured.php
+++ catalog/controller/extension/module/featured.php
@@ -80,7 +80,7 @@ class ControllerExtensionModuleFeatured extends Controller {
                                                 'thumb'       => $image,
                                                 'name'        => $product_info['name'],
                                                 'description_short' => $product_info['description_short'],
-                                                'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')) . '...',
+                                                'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get($this->config->get('config_theme') . '_product_description_length')),
                                                 'price'       => $price,
                                                 'special'     => $special,
                                                 'tax'         => $tax,
