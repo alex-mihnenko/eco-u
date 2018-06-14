bbe889bb4bb957bffdbcf97735964e2787a61cee
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-06-14 10:18:48 +0300

    Fix change cod to rbs payment

--- catalog/controller/ajax/index.php
+++ catalog/controller/ajax/index.php
@@ -1416,8 +1416,9 @@ class ControllerAjaxIndex extends Controller {
         // Create payment link
           $this->load->model('checkout/order');
 
-          $this->model_checkout_order->addDetailPayment($order_id, $this->config->get('config_payment_status_id'), false);
           $rbsid = $this->model_checkout_order->generateUniqRbsId($order_id);
+          
+          $this->model_checkout_order->addDetailPayment($order_id, $this->config->get('config_payment_status_id'), false);
           $this->model_checkout_order->setPaymentCustomField($order_id, $rbsid);
 
           $results = $this->load->controller('extension/payment/rbs/payment', $rbsid);
