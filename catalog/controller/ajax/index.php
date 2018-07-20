a8e1e9c2c8fc0448f236ef53a38ce96e5dbf8ef3
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-07-20 16:43:45 +0300

    Fixed - Add order

--- catalog/controller/ajax/index.php
+++ catalog/controller/ajax/index.php
@@ -555,7 +555,17 @@ class ControllerAjaxIndex extends Controller {
                   $data['products'][$i]['variant'] = $arWeightVariants[$product['weight_variant']];
                   
               }
+
               $data['products'][$i]['quantity'] = $product['quantity']*$product['packaging'];
+              
+              // For pieces
+                if($product['weight_class_id'] == 1) {
+
+                  if( $product['quantity'] > $product['packaging'] ) { $data['products'][$i]['amount'] = $product['quantity']; $data['products'][$i]['quantity'] = 1; }
+                  else if( $product['quantity'] < $product['packaging'] ) { $data['products'][$i]['amount'] = $product['packaging']; $data['products'][$i]['quantity'] = 1; }
+                  
+                }
+              // ---
 
               $total += ($this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax')) * $product['quantity']);
           }
