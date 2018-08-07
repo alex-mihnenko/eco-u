b1d792ec07795796da735a2b2f83f378ae2eda11
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-08-07 12:46:57 +0300

    Added intercom exchange

--- webhooks/crm-change-order.php
+++ webhooks/crm-change-order.php
@@ -25,7 +25,7 @@
 	$data = array('apiKey' => RCRM_KEY, 'by' => 'id');
 	$result = connectGetAPI($url, $data);
 
-	if( !isset($result->order) || !isset($result->order->externalId) ) {
+	if( !isset($result->order) || !isset($result->order->externalId) || !isset($result->order->customer) ) {
 		// ---
 			$log[] = 'No CRM order';
 
@@ -36,6 +36,7 @@
 	}
 
 	$order = $result->order;
+	$customer = $result->customer;
 	
 	if( $order->status != 'confim' ){
 		// ---
@@ -63,31 +64,32 @@
 	// ---
 
 
-	// Save order adddress
+	// Save adddress
 		$row_order = $rows_order->fetch_assoc();
 
-		// Fixed address
-			$address = '';
+
+		// Get order address
+			$order_address = '';
 
 			if( isset($order->delivery->address) ){
 				// ---
 					// Region and City
-					if( isset($order->delivery->address->cityType) ) { $address .= $order->delivery->address->cityType.' '; }
-					else if( isset($order->delivery->address->region) ) { $address .= $order->delivery->address->region.', '; }
-					if( isset($order->delivery->address->city) ) { $address .= $order->delivery->address->city.', '; }
+					if( isset($order->delivery->address->cityType) ) { $order_address .= $order->delivery->address->cityType.' '; }
+					else if( isset($order->delivery->address->region) ) { $order_address .= $order->delivery->address->region.', '; }
+					if( isset($order->delivery->address->city) ) { $order_address .= $order->delivery->address->city.', '; }
 
 					// Street
-					if( isset($order->delivery->address->streetType) ) { $address .= $order->delivery->address->streetType.' '; }
-					if( isset($order->delivery->address->street) ) { $address .= $order->delivery->address->street.', '; }
+					if( isset($order->delivery->address->streetType) ) { $order_address .= $order->delivery->address->streetType.' '; }
+					if( isset($order->delivery->address->street) ) { $order_address .= $order->delivery->address->street.', '; }
 
 					// Add
-					if( isset($order->delivery->address->building) ) { $address .= 'д. '.$order->delivery->address->building.', '; }
-					if( isset($order->delivery->address->flat) ) { $address .= 'кв./офис '.$order->delivery->address->flat.', '; }
-					if( isset($order->delivery->address->block) ) { $address .= 'под. '.$order->delivery->address->block.', '; }
-					if( isset($order->delivery->address->floor) ) { $address .= 'эт. '.$order->delivery->address->floor.', '; }
+					if( isset($order->delivery->address->building) ) { $order_address .= 'д. '.$order->delivery->address->building.', '; }
+					if( isset($order->delivery->address->flat) ) { $order_address .= 'кв./офис '.$order->delivery->address->flat.', '; }
+					if( isset($order->delivery->address->block) ) { $order_address .= 'под. '.$order->delivery->address->block.', '; }
+					if( isset($order->delivery->address->floor) ) { $order_address .= 'эт. '.$order->delivery->address->floor.', '; }
 
 					// Fix
-					$address = mb_substr($address,0,mb_strlen($address)-2);
+					$order_address = mb_substr($order_address,0,mb_strlen($order_address)-2);
 				// ---
 			}
 		// ---
@@ -121,79 +123,105 @@
 				// ---
 
 				// To CRM
-					// Edit customer
-						if( isset($order->customer) ) {
-							// ---
-								$customer = $order->customer;
+					$customerData = array();
 
-								$customerData = array();
-								$customerAddress = array();
-								$customerCustomFields = array();
+					if( !isset($customer->address) ){
+						// ---
+							$customerAddress = array();
+							
+							if( isset($order->delivery->address->countryIso) ) { $customerAddress['countryIso'] = $order->delivery->address->countryIso; }
+							if( isset($order->delivery->address->region) ) { $customerAddress['region'] = $order->delivery->address->region; }
+							if( isset($order->delivery->address->regionId) ) { $customerAddress['regionId'] = $order->delivery->address->regionId; }
+							if( isset($order->delivery->address->city) ) { $customerAddress['city'] = $order->delivery->address->city; }
+							if( isset($order->delivery->address->cityId) ) { $customerAddress['cityId'] = $order->delivery->address->cityId; }
+							if( isset($order->delivery->address->cityType) ) { $customerAddress['cityType'] = $order->delivery->address->cityType; }
+							if( isset($order->delivery->address->street) ) { $customerAddress['street'] = $order->delivery->address->street; }
+							if( isset($order->delivery->address->streetId) ) { $customerAddress['streetId'] = $order->delivery->address->streetId; }
+							if( isset($order->delivery->address->streetType) ) { $customerAddress['streetType'] = $order->delivery->address->streetType; }
+							if( isset($order->delivery->address->building) ) { $customerAddress['building'] = $order->delivery->address->building; }
+							if( isset($order->delivery->address->flat) ) { $customerAddress['flat'] = $order->delivery->address->flat; }
+							if( isset($order->delivery->address->intercomCode) ) { $customerAddress['intercomCode'] = $order->delivery->address->intercomCode; }
+							if( isset($order->delivery->address->floor) ) { $customerAddress['floor'] = $order->delivery->address->floor; }
+							if( isset($order->delivery->address->block) ) { $customerAddress['block'] = $order->delivery->address->block; }
+							if( isset($order->delivery->address->house) ) { $customerAddress['house'] = $order->delivery->address->house; }
+							if( isset($order->delivery->address->metro) ) { $customerAddress['metro'] = $order->delivery->address->metro; }
 
+							$customerData['address'] = $customerAddress;
+						// ---
+					}
+					else{
+						// ---
+							$customerCustomFields = array();
 
-								if( !isset($customer->address) ){
-									// ---
-										if( isset($order->delivery->address->countryIso) ) { $customerAddress['countryIso'] = $order->delivery->address->countryIso; }
-										if( isset($order->delivery->address->region) ) { $customerAddress['region'] = $order->delivery->address->region; }
-										if( isset($order->delivery->address->regionId) ) { $customerAddress['regionId'] = $order->delivery->address->regionId; }
-										if( isset($order->delivery->address->city) ) { $customerAddress['city'] = $order->delivery->address->city; }
-										if( isset($order->delivery->address->cityId) ) { $customerAddress['cityId'] = $order->delivery->address->cityId; }
-										if( isset($order->delivery->address->cityType) ) { $customerAddress['cityType'] = $order->delivery->address->cityType; }
-										if( isset($order->delivery->address->street) ) { $customerAddress['street'] = $order->delivery->address->street; }
-										if( isset($order->delivery->address->streetId) ) { $customerAddress['streetId'] = $order->delivery->address->streetId; }
-										if( isset($order->delivery->address->streetType) ) { $customerAddress['streetType'] = $order->delivery->address->streetType; }
-										if( isset($order->delivery->address->building) ) { $customerAddress['building'] = $order->delivery->address->building; }
-										if( isset($order->delivery->address->flat) ) { $customerAddress['flat'] = $order->delivery->address->flat; }
-										if( isset($order->delivery->address->intercomCode) ) { $customerAddress['intercomCode'] = $order->delivery->address->intercomCode; }
-										if( isset($order->delivery->address->floor) ) { $customerAddress['floor'] = $order->delivery->address->floor; }
-										if( isset($order->delivery->address->block) ) { $customerAddress['block'] = $order->delivery->address->block; }
-										if( isset($order->delivery->address->house) ) { $customerAddress['house'] = $order->delivery->address->house; }
-										if( isset($order->delivery->address->metro) ) { $customerAddress['metro'] = $order->delivery->address->metro; }
+							if( !isset($customer->customFields->addition_address_first) && !isset($customer->customFields->addition_address_second) && !isset($customer->customFields->addition_address_third) ){
+								$customerCustomFields['addition_address_first'] = $address;
+							}
+							else if( isset($customer->customFields->addition_address_first) && !isset($customer->customFields->addition_address_second) && !isset($customer->customFields->addition_address_third) ){
+								$customerCustomFields['addition_address_second'] = $address;
+							}
+							else {
+								$customerCustomFields['addition_address_third'] = $address;
+							}
 
-										$customerData = array(
-											'address' => $customerAddress
-										);
-									// ---
-								}
-								else {
-									if( !isset($customer->customFields->addition_address_first) && !isset($customer->customFields->addition_address_second) && !isset($customer->customFields->addition_address_third) ){
-										$customerCustomFields['addition_address_first'] = $address;
-									}
-									else if( isset($customer->customFields->addition_address_first) && !isset($customer->customFields->addition_address_second) && !isset($customer->customFields->addition_address_third) ){
-										$customerCustomFields['addition_address_second'] = $address;
-									}
-									else {
-										$customerCustomFields['addition_address_third'] = $address;
-									}
-
-									$customerData = array(
-										'customFields' => $customerCustomFields
-									);
-								}
+							$customerData = array(
+								'customFields' => $customerCustomFields
+							);
+						// ---
+					}
 
 
+					$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
 
-								$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
+					
 
-								
+					$data = array(
+						'apiKey' => RCRM_KEY,
+						'by' => 'id',
+						'customer' => json_encode($customerData)
+					);
 
-								$data = array(
-									'apiKey' => RCRM_KEY,
-									'by' => 'id',
-									'customer' => json_encode($customerData)
-								);
+					$result = connectPostAPI($url, $data);
 
-								$result = connectPostAPI($url, $data);
-
-								$log[] = 'CRM customer address ['.$row_order['customer_id'].'] update: '.json_encode($result);
-							// ---
-						}
-					// ---
+					$log[] = 'CRM customer address ['.$row_order['customer_id'].'] update: '.json_encode($result);
 				// ---
 			}
 			else {
 				$log[] = 'OC customer address ['.$row_order['customer_id'].'] already exist';
 			}
+		// ---
+	// ---
+
+	// Save custom fields
+		// Get order custom fields
+			$order_intercom = '';
+
+			if( isset($order->customFields->intercom) ){
+				$order_intercom = $order->customFields->intercom;
+			}
+		// ---
+
+		// Save custom fields
+			$customerCustomFields = array();
+
+			if( $order_intercom != '' ){
+				// ---
+					$customerCustomFields['intercom'] = $order_intercom;
+				// ---
+			}
+
+
+			$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
+
+								
+			$data = array(
+				'apiKey' => RCRM_KEY,
+				'by' => 'id',
+				'customer' => json_encode($customerData)
+			);
+
+			$result = connectPostAPI($url, $data);
+
+			$log[] = 'CRM customer address ['.$row_order['customer_id'].'] update: '.json_encode($result);
 		// ---
 	// ---
 // ---
