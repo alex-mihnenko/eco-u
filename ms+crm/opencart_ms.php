08609297b5e60d7db869654851a5437487bc9f8d
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-08-22 14:56:27 +0300

    Fixed ms+crm/opencart_ms.php

--- ms+crm/opencart_ms.php
+++ ms+crm/opencart_ms.php
@@ -368,7 +368,7 @@ if($argv[1]=='2'){
 								// ---
 
 								// Discount
-									if( $product_discount >= 0 ){
+									if( $product_discount > 0 ){
 										// ---
 											$special_price = $price - ($price * (intval($product_discount)/100));
 
@@ -379,6 +379,14 @@ if($argv[1]=='2'){
 												WHERE product_id='".$product_id."'
 											");
 										// ---
+									}
+									else {
+										mysql_query("
+											UPDATE `oc_product` SET 
+											`special_price`='',
+											`discount`='0' 
+											WHERE product_id='".$product_id."'
+										");
 									}
 								// ---
 
