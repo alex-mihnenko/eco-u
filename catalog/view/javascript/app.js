c54e0fb2c52bb37cd1a35709d79a31f57f45bb91
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-08-23 13:40:03 +0300

    Fixed dynamic tooltips

--- catalog/view/javascript/app.js
+++ catalog/view/javascript/app.js
@@ -60,6 +60,7 @@ $(document).ready(function() {
 
 		// Catalog			
 			scrollSpyCategory($(window).scrollTop());
+			initTooltips();
 		// ---
 	// ---
 
@@ -775,6 +776,7 @@ $(document).ready(function() {
 			                    pElement.append(products);
 			                    lazyLoadImages(pElement);
 			                    catalogProductDymanmicShow(pElement);
+			                    initTooltips();
 			                }
 		                // ---
 		            });
@@ -1388,76 +1390,6 @@ $(document).ready(function() {
 				$(this).next().not(":visible").slideDown().prev().addClass("active");
 			});
 		// ---
-
-		// Tooltips
-			$(function() {
-				var targets = $( '[rel~=tooltip]' ),
-					target  = false,
-					tooltip = false,
-					title   = false;
-
-					targets.bind( 'mouseenter', function() {
-					target  = $( this );
-					tip     = target.attr( 'title' );
-					tooltip = $( '<div id="tooltip"></div>' );
-					if( !tip || tip == '' ) return false;
-
-					target.removeAttr( 'title' );
-					tooltip.css( 'opacity', 0 ).html( tip ).appendTo( 'body' );
-
-					var init_tooltip = function() {
-						if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-							tooltip.css( 'max-width', $( window ).width() / 2 );
-						else
-							tooltip.css( 'max-width', 340 );
-			 
-						var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-							pos_top  = target.offset().top - tooltip.outerHeight() + 113;
-			 
-						if( pos_left < 0 )
-						{
-							pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-							tooltip.addClass( 'left' );
-						}
-						else
-							tooltip.removeClass( 'left' );
-			 
-						if( pos_left + tooltip.outerWidth() > $( window ).width() )
-						{
-							pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-							tooltip.addClass( 'right' );
-						}
-						else
-							tooltip.removeClass( 'right' );
-			 
-						if( pos_top < 0 )
-						{
-							var pos_top  = target.offset().top + target.outerHeight();
-							tooltip.addClass( 'top' );
-						}
-						else
-							tooltip.removeClass( 'top' );
-			 
-						tooltip.css( { left: pos_left, top: pos_top } )
-							   .animate( { top: '+=10', opacity: 1 }, 50 );
-					};
-			 
-					init_tooltip();
-					$( window ).resize( init_tooltip );
-			 
-					var remove_tooltip = function(){
-						tooltip.animate( { top: '-=10', opacity: 0 }, 50, function() {
-							$( this ).remove();
-						});
-			 
-						target.attr( 'title', tip );
-					};
-			 
-					target.bind( 'mouseleave', remove_tooltip );
-					tooltip.bind( 'click', remove_tooltip );
-				});
-			});
-		// ---
 	// ---
 });
 
@@ -1798,6 +1730,7 @@ $(document).ready(function() {
 
 			                    catalogProductDymanmicShow($('#search-content'));
 			                    lazyLoadImages($('#search-content'));
+			                    initTooltips();
 							// ---
                     	});
 					}
@@ -2011,4 +1944,78 @@ $(document).ready(function() {
 	  var match = regex.exec(url);
 	  return match ? match[1] : "";
 	}
+// ---
+
+// Components
+	// Tooltips
+		function initTooltips() {
+			// ---
+				var targets = $( '[rel~=tooltip]' ),
+					target  = false,
+					tooltip = false,
+					title   = false;
+
+					targets.bind( 'mouseenter', function() {
+					target  = $( this );
+					tip     = target.attr( 'title' );
+					tooltip = $( '<div id="tooltip"></div>' );
+					if( !tip || tip == '' ) return false;
+
+					target.removeAttr( 'title' );
+					tooltip.css( 'opacity', 0 ).html( tip ).appendTo( 'body' );
+
+					var init_tooltip = function() {
+						if( $( window ).width() < tooltip.outerWidth() * 1.5 )
+							tooltip.css( 'max-width', $( window ).width() / 2 );
+						else
+							tooltip.css( 'max-width', 340 );
+			 
+						var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
+							pos_top  = target.offset().top - tooltip.outerHeight() + 113;
+			 
+						if( pos_left < 0 )
+						{
+							pos_left = target.offset().left + target.outerWidth() / 2 - 20;
+							tooltip.addClass( 'left' );
+						}
+						else
+							tooltip.removeClass( 'left' );
+			 
+						if( pos_left + tooltip.outerWidth() > $( window ).width() )
+						{
+							pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
+							tooltip.addClass( 'right' );
+						}
+						else
+							tooltip.removeClass( 'right' );
+			 
+						if( pos_top < 0 )
+						{
+							var pos_top  = target.offset().top + target.outerHeight();
+							tooltip.addClass( 'top' );
+						}
+						else
+							tooltip.removeClass( 'top' );
+			 
+						tooltip.css( { left: pos_left, top: pos_top } )
+							   .animate( { top: '+=10', opacity: 1 }, 50 );
+					};
+			 
+					init_tooltip();
+					$( window ).resize( init_tooltip );
+			 
+					var remove_tooltip = function(){
+						tooltip.animate( { top: '-=10', opacity: 0 }, 50, function() {
+							$( this ).remove();
+						});
+			 
+						target.attr( 'title', tip );
+					};
+			 
+					target.bind( 'mouseleave', remove_tooltip );
+					tooltip.bind( 'click', remove_tooltip );
+				});
+			// ---
+		}
+	// ---
 // ---
