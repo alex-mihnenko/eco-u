a21251ff582106f35afb8193cac3b4358250dcf5
Vyacheslav Turchin <mr.tur46in@yandex.ru>
2018-08-27 16:55:44 +0300

    Fixed cron/crm-to-oc-orders.php

--- cron/crm-to-oc-orders.php
+++ cron/crm-to-oc-orders.php
@@ -35,149 +35,153 @@
 		// ---
 			$customer = $order->customer;
 
-			// Check
-				$q = "SELECT * FROM `".DB_PREFIX."order` WHERE `order_id`='".$order->externalId."' LIMIT 1;";
-				$rows_order = $db->query($q);
+			if( isset($order->externalId) ){
+				// Check
+					$q = "SELECT * FROM `".DB_PREFIX."order` WHERE `order_id`='".$order->externalId."' LIMIT 1;";
+					$rows_order = $db->query($q);
 
-				if ($rows_order->num_rows == 0) {
-					$log[] = 'No OC order';
+					if ($rows_order->num_rows == 0) {
+						$log[] = 'No OC order';
 
-					$res['log'] = $log;
-					$res['mess']='Success';
-					echo json_encode($res); exit;
-				}
-
-				$row_order = $rows_order->fetch_assoc();
-			// ---
-
-			// Save adddress
-				// Get order address
-					if( isset($order->delivery->address) ){
-						// ---
-							if( isset($order->customFields->order_delivery_address_type) && $order->customFields->order_delivery_address_type != false ){
-								$order_address = addressCrmToOc($order->delivery->address, true);
-							}
-							else {
-								$order_address = addressCrmToOc($order->delivery->address, false);
-							}
-						// ---
+						$res['log'] = $log;
+						$res['mess']='Success';
+						echo json_encode($res); exit;
 					}
+
+					$row_order = $rows_order->fetch_assoc();
 				// ---
-					
-				// Save address
-					// To CRM
-						if( isset($customer->address) ){
-							// Clear main address
-		                    	$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
 
-		                    	$data = array(
-		                        	'apiKey' => RCRM_KEY,
-		                        	'by' => 'id',
-		                        	'customer' => json_encode(array('address' => array()))
-		                    	);
-
-		                    	$result = connectPostAPI($url, $data);
-
-								$log[] = 'CRM customer address ['.$row_order['customer_id'].'] delete: '.json_encode($result);
-		                  	// ---
-						}
-
-						$customerData = array();
-
-						// Set data
-							$customerData['address'] = $order_address['obj'];
-
-							if( isset($order->customFields->order_delivery_address_type) ){
-								$customerCustomFields = array();
-
-								$customerCustomFields['customer_delivery_address_type'] = $order->customFields->order_delivery_address_type;
-								
-								$customerData['customFields'] = $customerCustomFields;
-							}
-						// ---
-
-						
-						// Save  address
-							$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
-
-							$data = array(
-								'apiKey' => RCRM_KEY,
-								'by' => 'id',
-								'customer' => json_encode($customerData)
-							);
-
-							$result = connectPostAPI($url, $data);
-
-							$log[] = 'CRM customer address ['.$row_order['customer_id'].'] update: '.json_encode($result);
-						// ---
-					// ---
-
-					// To OC
-						// Edit customer addresses
-							$q = "
-								UPDATE `".DB_PREFIX."address` SET 
-								`custom_field` = '' 
-								WHERE `customer_id`='".$row_order['customer_id']."'
-							;";
-
-							if ($db->query($q) === TRUE) {
-							    $log[] = 'OC customer addresses ['.$row_order['customer_id'].'] has been updated';
-							} else {
-								$log[] = 'OC customer addresses ['.$row_order['order_id'].'] has been not updated: '.$db->error;
-							}
-						// ---
-
-						$oc_address_type = 'primary';
-
-						$q = "SELECT * FROM `".DB_PREFIX."address` WHERE `customer_id`='".$row_order['customer_id']."' AND `address_1` = '".$order_address['text']."';";
-						$rows_address = $db->query($q);
-
-						if ($rows_address->num_rows == 0 && !empty($order_address['text']) ) {
+				// Save adddress
+					// Get order address
+						if( isset($order->delivery->address) ){
 							// ---
-								$q = "
-									INSERT INTO `".DB_PREFIX."address` SET 
-									`customer_id` = '".$row_order['customer_id']."',
-									`firstname` = '".$row_order['firstname']."',
-									`lastname` = '".$row_order['lastname']."',
-									`company` = '',
-									`address_1` = '".$order_address['text']."',
-									`address_2` = '".json_encode($order_address['obj'],JSON_UNESCAPED_UNICODE)."',
-									`city` = '',
-									`postcode` = '',
-									`country_id` = '0',
-									`zone_id` = '0',
-									`custom_field` = '".$oc_address_type."'
-								";
-								
-								if ($db->query($q) === TRUE) {
-								    $log[] = 'OC customer address ['.$row_order['customer_id'].'] has been inserted';
-								} else {
-									$log[] = 'OC customer address ['.$row_order['customer_id'].'] has been not inserted: '.$db->error;
+								if( isset($order->customFields->order_delivery_address_type) && $order->customFields->order_delivery_address_type != false ){
+									$order_address = addressCrmToOc($order->delivery->address, true);
+								}
+								else {
+									$order_address = addressCrmToOc($order->delivery->address, false);
 								}
 							// ---
 						}
-						else {
-							$row_address = $rows_address->fetch_assoc();
+					// ---
+						
+					// Save address
+						// To CRM
+							if( isset($customer->address) ){
+								// Clear main address
+			                    	$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
 
-							$q = "
-								UPDATE `".DB_PREFIX."address` SET 
-								`address_1` = '".$order_address['text']."',
-								`address_2` = '".json_encode($order_address['obj'],JSON_UNESCAPED_UNICODE)."',
-								`custom_field` = '".$oc_address_type."' 
-								WHERE `customer_id`='".$row_order['customer_id']."' AND `address_id`='".$row_address['address_id']."'
-							;";
+			                    	$data = array(
+			                        	'apiKey' => RCRM_KEY,
+			                        	'by' => 'id',
+			                        	'customer' => json_encode(array('address' => array()))
+			                    	);
 
-							if ($db->query($q) === TRUE) {
-							    $log[] = 'OC customer addresses ['.$row_order['customer_id'].'] has been updated';
-							} else {
-								$log[] = 'OC customer addresses ['.$row_order['order_id'].'] has been not updated: '.$db->error;
+			                    	$result = connectPostAPI($url, $data);
+
+									$log[] = 'CRM customer address ['.$row_order['customer_id'].'] delete: '.json_encode($result);
+			                  	// ---
 							}
 
-							$log[] = 'OC customer address ['.$row_order['customer_id'].'] already exist or empty';
-						}
+							$customerData = array();
+
+							// Set data
+								$customerData['address'] = $order_address['obj'];
+
+								if( isset($order->customFields->order_delivery_address_type) ){
+									$customerCustomFields = array();
+
+									$customerCustomFields['customer_delivery_address_type'] = $order->customFields->order_delivery_address_type;
+									
+									$customerData['customFields'] = $customerCustomFields;
+								}
+							// ---
+
+							
+							// Save  address
+								$url = 'https://eco-u.retailcrm.ru/api/v5/customers/'.$customer->id.'/edit';
+
+								$data = array(
+									'apiKey' => RCRM_KEY,
+									'by' => 'id',
+									'customer' => json_encode($customerData)
+								);
+
+								$result = connectPostAPI($url, $data);
+
+								$log[] = 'CRM customer address ['.$row_order['customer_id'].'] update: '.json_encode($result);
+							// ---
+						// ---
+
+						// To OC
+							// Edit customer addresses
+								$q = "
+									UPDATE `".DB_PREFIX."address` SET 
+									`custom_field` = '' 
+									WHERE `customer_id`='".$row_order['customer_id']."'
+								;";
+
+								if ($db->query($q) === TRUE) {
+								    $log[] = 'OC customer addresses ['.$row_order['customer_id'].'] has been updated';
+								} else {
+									$log[] = 'OC customer addresses ['.$row_order['order_id'].'] has been not updated: '.$db->error;
+								}
+							// ---
+
+							$oc_address_type = 'primary';
+
+							$q = "SELECT * FROM `".DB_PREFIX."address` WHERE `customer_id`='".$row_order['customer_id']."' AND `address_1` = '".$order_address['text']."';";
+							$rows_address = $db->query($q);
+
+							if ($rows_address->num_rows == 0 && !empty($order_address['text']) ) {
+								// ---
+									$q = "
+										INSERT INTO `".DB_PREFIX."address` SET 
+										`customer_id` = '".$row_order['customer_id']."',
+										`firstname` = '".$row_order['firstname']."',
+										`lastname` = '".$row_order['lastname']."',
+										`company` = '',
+										`address_1` = '".$order_address['text']."',
+										`address_2` = '".json_encode($order_address['obj'],JSON_UNESCAPED_UNICODE)."',
+										`city` = '',
+										`postcode` = '',
+										`country_id` = '0',
+										`zone_id` = '0',
+										`custom_field` = '".$oc_address_type."'
+									";
+									
+									if ($db->query($q) === TRUE) {
+									    $log[] = 'OC customer address ['.$row_order['customer_id'].'] has been inserted';
+									} else {
+										$log[] = 'OC customer address ['.$row_order['customer_id'].'] has been not inserted: '.$db->error;
+									}
+								// ---
+							}
+							else {
+								$row_address = $rows_address->fetch_assoc();
+
+								$q = "
+									UPDATE `".DB_PREFIX."address` SET 
+									`address_1` = '".$order_address['text']."',
+									`address_2` = '".json_encode($order_address['obj'],JSON_UNESCAPED_UNICODE)."',
+									`custom_field` = '".$oc_address_type."' 
+									WHERE `customer_id`='".$row_order['customer_id']."' AND `address_id`='".$row_address['address_id']."'
+								;";
+
+								if ($db->query($q) === TRUE) {
+								    $log[] = 'OC customer addresses ['.$row_order['customer_id'].'] has been updated';
+								} else {
+									$log[] = 'OC customer addresses ['.$row_order['order_id'].'] has been not updated: '.$db->error;
+								}
+
+								$log[] = 'OC customer address ['.$row_order['customer_id'].'] already exist or empty';
+							}
+						// ---
 					// ---
 				// ---
-			// ---
+
+				$count++;
+			}
 		// ---
 	}
 
